02645fa1cf35104df4054a9af1a5458912b4af3a
steph-cs <61119367+steph-cs@users.noreply.github.com>
2021-11-21 19:06:34 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -1,9 +1,9 @@
 
 
 class Jogador:
-    def __init__(self, peca:str):
+    def __init__(self, peca:int):
         self._peca = peca
-        self._nome = ("Jogador 1" if peca == "t1" else "Jogador 2")
+        self._nome = ("Jogador 1" if peca == 1 else "Jogador 2")
         self._totalPecas = 18
 
     @property
